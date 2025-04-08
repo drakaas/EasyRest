222dcf9a05f324b6749d8dacae0f38502658865e
Almamoun Oussama <oussama1.alm@gmail.com>
2025-04-08 15:45:07 +0100

    test

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const init =require("./src/server");
 
 
 function main() {
-    let app = init(3000,(port)=>{console.log("app listening on port "+port)});
+    let app = init(5000,(port)=>{console.log("app listening on port "+port)});
  }
  
  main();
