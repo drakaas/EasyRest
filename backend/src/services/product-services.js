e3515e6ba373d7055bb650bbb01b65a173d1cdee
Almamoun Oussama <oussama1.alm@gmail.com>
2025-04-08 16:21:24 +0100

    test

--- backend/src/services/product-services.js
+++ backend/src/services/product-services.js
@@ -3,10 +3,10 @@ const {categoryBySlug,categoryById}=require("../dao/productCategory-dao");
 const ProductByCategorySlug= async(req,res)=>{
      try {
           const slug = req.params.slug;
+          console.log(slug)
           if (!slug) return res.status(404).send({message:"erreur categorie introuvable"});
           let categorie = await categoryBySlug(slug);
           if(categorie.message) return res.status(500).send(categorie.message);
-
           let products=await  getProductCategory(categorie._id );
           if(products.message) return res.status(500).send(categorie.message);
           return res.status(200).send(products);
