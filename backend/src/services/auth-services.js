28a79900b40d91e6915f4a408bbf538d56b107eb
Almamoun Oussama <oussama1.alm@gmail.com>
2025-04-14 05:38:16 +0100

    tet

--- backend/src/services/auth-services.js
+++ backend/src/services/auth-services.js
@@ -16,7 +16,7 @@ const register = async (req, res) => {
 
     // Create and save new user
     const user = insertUser({email,password,username:name})
-    if(user.message) return res.status(500).json({message:user.message});
+    if(user.message!=null) return res.status(500).json({message:user.message});
     const payload = { id: user._id, email: user.email };
     const token = jwt.sign(payload, process.env.JWT_SECRET , { expiresIn: '7d' });
 
