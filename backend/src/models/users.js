01100106b45a8f7d84e4f534e82d58584e390537
Almamoun Oussama <oussama1.alm@gmail.com>
2025-04-14 05:28:59 +0100

    d

--- backend/src/models/users.js
+++ backend/src/models/users.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const validator = require('validator');
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
-const validator = require('validator');
+
 
 const userSchema = new mongoose.Schema({
   username: {
