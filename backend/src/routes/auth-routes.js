c20b87780b23b95525a1dbad6657246011aacc07
Almamoun Oussama <oussama1.alm@gmail.com>
2025-04-14 06:10:15 +0100

    test oauth

--- backend/src/routes/auth-routes.js
+++ backend/src/routes/auth-routes.js
@@ -3,6 +3,7 @@ const router = Router()
 // const service = require("../services/auth-services")
 const passport = require("passport")
 const jwt = require("jsonwebtoken");
+let dotenv = require('dotenv').config()
 const {register,login} =require("../services/auth-services")
 
 router.post("/register",register)
@@ -16,26 +17,46 @@ router.post("/login",login)
 router.get('/google',passport.authenticate("google",{scope:['email','profile']}))
 
 
-router.get('/google/callback', 
-    passport.authenticate('google', { failureRedirect: '/login' }), 
+// router.get('/google/callback', 
+//     passport.authenticate('google', { failureRedirect: '/login' }), 
+//     (req, res) => {
+//       const user = req.user;
+//       const token = jwt.sign({ id: user.id, name: user.displayName, email: user.emails[0].value },  process.env.JWT_SECRET, {
+//         expiresIn: "1h",
+//       });
+//       console.log(token);
+//       res.cookie("token", token, { httpOnly: true, secure: false });
+//       res.redirect("http://localhost:5173/dashboard");
+//     }
+//   );
+  router.get(
+    "/google/callback",
+    passport.authenticate("google", { session: false }),
     (req, res) => {
       const user = req.user;
-      const token = jwt.sign({ id: user.id, name: user.displayName, email: user.emails[0].value }, "your_jwt_secret", {
-        expiresIn: "1h",
+      const token = jwt.sign({ id: user.id, name: user.displayName, email: user.emails[0].value },  process.env.JWT_SECRET, {
+        expiresIn: "7d",
       });
-      console.log(token);
-      res.cookie("token", token, { httpOnly: true, secure: false });
-      res.redirect("http://localhost:5173/dashboard");
+      res.json({
+        success: true,
+        message: "Login successful",
+        token: token,
+        user: {
+          id: user.id,
+          username: user.username,
+          email: user.email,
+          role: user.role,
+        },
+      });
     }
   );
-
   router.get("/me",(req, res) => {
     const token = req.cookies.csrftoken;
     console.log(req.cookies);
     if (!token) return res.status(401).json({ authenticated: false });
   
     try {
-      const decoded = jwt.verify(token, "your_jwt_secret");
+      const decoded = jwt.verify(token,  process.env.JWT_SECRET);
       res.json({ authenticated: true, user: decoded });
     } catch (err) {
       res.status(401).json({ authenticated: false });
