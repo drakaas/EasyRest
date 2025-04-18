3d5e149632643ff1556291f35bdf2d374608206d
drakaas <oussama1.alm@gmail.com>
2025-04-18 15:10:18 +0100

    d

--- backend/src/models/Cart.js
+++ backend/src/models/Cart.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const cartItemSchema = new mongoose.Schema({
   product: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'Product',
+    ref: 'products',
     required: [true, 'Cart item must reference a product']
   },
   quantity: {
