09c44d0723caca0ee9c3201728c44c17fdb19e11
drakaas <oussama1.alm@gmail.com>
2025-04-21 05:55:10 +0100

    d

--- backend/src/dao/product-dao.js
+++ backend/src/dao/product-dao.js
@@ -17,16 +17,19 @@ const getProductCategory = async(id)=>{
           if(!id) return {message:" erreur aucune categorie n'a été selectionnée"};
           console.log(id)
           console.log("before problem")
-          const products = await Product.find({ slug: id})
+          const products = await Product.find({
+               $or: [
+                    { slug: id },
+                    { category: id }
+               ]
+          })
           .populate('category')
           .sort({ createdAt: -1 });
           console.log("after problem")
           if (!products ) return {message :"aucun produit n'a été trouvé"}
           return products;
-          
      } catch (error) {
           return {message:"error "+error.message}
-
      }
 }
 
